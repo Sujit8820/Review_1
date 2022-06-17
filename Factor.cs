168997d0eed14dbf15a4367bda6150768dd61238
Sujit8820 <kadamsujit8820@gmail.com>
2022-06-17 15:36:21 +0530

    [Sujit]Add: Review first Factorial Problem

---
+++ Factor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    public class Factor
+    {
+        public void Factors_Of_Number()
+        {
+            Console.Write("Enter Number: ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nFactors as below");
+
+            for(int i = 1; i < Num; i++)
+            {
+                if(Num % i == 0)
+                {
+                    Num = Num / i;
+                    Console.WriteLine(i);
+                }
+               
+            }
+                Console.WriteLine(Num); 
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Factorial
+{
+    public class Factors
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Wellcome to factorial program\n");
+
+            Factor factor = new Factor();
+            factor.Factors_Of_Number();
+
+
+        }
+    }
+     
+}
